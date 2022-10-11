78a630de2c9115131598e813697350b09acf20bf
Adonai Domingez <donihernandez003@gmail.com>
2022-10-10 22:45:56 -0400

    feat(replace-material-ui): replace Material UI with Chakra UI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,13 @@
-import '../styles/globals.css';
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
-
 import type { AppProps } from 'next/app';
+import { ChakraProvider } from '@chakra-ui/react';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+    return (
+        <ChakraProvider>
+            <Component {...pageProps} />
+        </ChakraProvider>
+    );
 }
 
 export default MyApp;
